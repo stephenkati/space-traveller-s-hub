4c3eb86d63d2b62a1e582849195a87cb6cb5372d
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-05-16 11:28:35 +0200

    adds Navbar to the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Missions from './components/Pages/Mission';
+import Rockets from './components/Pages/Rocket';
+import Profile from './components/Pages/Profile';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </>
+);
 
 export default App;
---
+++ src/components/Pages/Rocket.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rocket = () => (
+  <div>Rocket</div>
+);
+
+export default Rocket;
