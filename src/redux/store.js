ffc2eb8e19d12630fe7956ab857398df8d849b38
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-05-18 12:59:28 +0200

    fetch the rochets from the api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import MissionReducer from './missions/MissionSlice';
+import rocketsReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
     mission: MissionReducer,
+    rocket: rocketsReducer,
   },
 });
 
